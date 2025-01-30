eecacb6059acfc1741a7bd13bef3ba2361580167
mohamedElmesery999 <mohamedelmesery18@gmail.com>
2025-01-30 10:51:56 +0200

    isloading

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -16,7 +16,7 @@ export default function Register() {
     setIsLoading(true)
     const {data} = await axios.post("https://ecommerce.routemisr.com/api/v1/auth/signup", values);
     console.log(data);
-    setIsLoading(true)
+    setIsLoading(false)
     
   };
 
